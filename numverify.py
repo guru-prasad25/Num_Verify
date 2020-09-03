3e47a77f4253169fb069d20f2ac24e5b66cf6cbd
guru-prasad25 <67254031+guru-prasad25@users.noreply.github.com>
2020-09-03 18:54:57 +0530

    Create numverify.py

---
+++ numverify.py
@@ -0,0 +1,22 @@
+import requests
+#accesskey is the pyhton script used to save the api keys. Please dont hardcode your api keys, especially if your code is opensource.
+import accesskey 
+from ipstack import GeoLookup
+import json
+import currency_converter
+
+def num_verify():
+    phone_number = input("Please enter your phone number: ")
+
+
+
+
+    print(f"Your phone number is {phone_number}")
+
+    url = 'http://apilayer.net/api/validate?access_key=' + accesskey.access_key_num + '&number=' + phone_number
+    response = requests.get(url)
+    print (response.content)
+
+    resp = json.loads(response.content)
+    if resp['valid']:
+        print("the number is valid")
